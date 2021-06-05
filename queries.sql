c61c822f670b16f26c7fcabc1c3a33598beda109
RileyCC56 <rileycorpac@gmail.com>
2021-06-05 01:40:16 -0400

    CODE PUSH

---
+++ queries.sql
@@ -0,0 +1,37 @@
+Create table employees(
+	emp_no INT NOT NULL,
+	birth_date DATE Not Null,
+	first_name VARCHAR Not Null,
+	last_name VARCHAR Not Null,
+	gender VARCHAR Not Null,
+	hire_date DATE Not Null,
+	Primary Key (emp_no)
+	);
+	
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT count (first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info
